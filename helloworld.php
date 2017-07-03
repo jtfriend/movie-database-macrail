ecc9b4d5d13789868c68ab852f204f7a58932cde
Joseph Friend <jt2friend@gmail.com>
2017-07-03 20:52:03 +0100

    log and some simple start up files

---
+++ helloworld.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <title>Hello World</title>
+  </head>
+  <body>
+    <?php
+    //comments
+    ?>
+    <?php echo "Hello World!"; ?><br />
+  </body>
+</html>  
---
+++ testmysql.php
@@ -0,0 +1,15 @@
+<?php
+/*    Using "mysqli" instead of "mysql" that is obsolete.
+*     Utilisation de "mysqli" � la place de "mysql" qui est obsol�te.
+* Change the value of parameter 3 if you have set a password on the root userid
+* Changer la valeur du 3e param�tre si vous avez mis un mot de passe � root
+*/
+$mysqli = new mysqli('127.0.0.1', 'root', 'password');
+
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+}
+echo 'Connection OK';
+$mysqli->close();
+?>
